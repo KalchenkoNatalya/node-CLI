8d079bd5875e23ad973ae1a506b168e1b10664a0
KalchenkoNatalya <janatka2222@gmail.com>
2023-08-27 15:41:31 +0300

    step 3-6

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
+import yargs from "yargs";
 import contactsServices from "./contacts.js";
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsServices.listContacts();
-      return console.log(allContacts);
-    case "getById":
+      return console.table(allContacts);
+      break;
+    case "get":
       const contctById = await contactsServices.getContactById(id);
       return console.log(contctById);
-    case "addContact":
+      break;
+    case "add":
       const addContact = await contactsServices.addContact({
         name,
         email,
         phone,
       });
       return console.log(addContact);
-    case "deleteById":
+      break;
+    case "remove":
       const deleteContact = await contactsServices.removeContact(id);
       return console.log(deleteContact);
+      break;
 
     default:
-      console.log("Unknown action");
+      console.log("\x1B[31m Unknown action type!");
   }
 };
+const { argv } = yargs(process.argv.slice(2));
 
-// invokeAction({action: "list"})
-// invokeAction({action: "getById", id: "Z5sbDlS7pCzNsnAHLtDJd"})
-// invokeAction({action: "addContact", name: "Natalia", email: "natalia@gmail.com", phone: "0966002560"})
-invokeAction({ action: "deleteById", id: "p0gM8ixmgLvI5QhIuDkx1" });
+invokeAction(argv);
